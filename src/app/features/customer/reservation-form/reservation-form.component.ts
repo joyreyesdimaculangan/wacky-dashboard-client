4a0e1b01f65057654df9f5fcb23129eb61671840
Joy Dimaculangan <joyreyesdimaculangan@gmail.com>
2024-10-02 01:35:21 +0800

    added stepper to the reservation form

--- src/app/features/customer/reservation-form/reservation-form.component.ts
+++ src/app/features/customer/reservation-form/reservation-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatStepperModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatInputModule],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.scss'
 })
@@ -26,6 +27,16 @@ export class ReservationFormComponent {
     cakeMessage: '',
     otherRequest: '',
   };
+  availableTimes = [
+    { id: '10-am', value: '10:00 AM', label: '10:00 AM' },
+    { id: '10-30am', value: '10:30 AM', label: '10:30 AM' },
+    { id: '11-am', value: '11:00 AM', label: '11:00 AM' },
+    { id: '3-00-pm', value: '3:00 PM', label: '3:00 PM' },
+    { id: '3-30-pm', value: '3:30 PM', label: '3:30 PM' },
+    { id: '4-00-pm', value: '4:00 PM', label: '4:00 PM' },
+  ];
+  isFirstStepComplete = false;
+  isSecondStepComplete = false;
   isReservationOpen = false;
 
   openReservationForm() {
@@ -34,6 +45,16 @@ export class ReservationFormComponent {
 
   closeReservationForm() {
     this.isReservationOpen = false;
+  }
+
+  goToNextStep() {
+    // Logic to move to the next step
+    this.isFirstStepComplete = true;
+  }
+
+  goToPreviousStep() {
+    // Logic to move back to the previous step
+    this.isFirstStepComplete = false;
   }
 
   @Output() reservationSubmitted = new EventEmitter<any>();
