2dea16ada6d9f14377b146b96fa8640ca818b3df
Mark Lowel Montealto <102800815+KuyaGit@users.noreply.github.com>
2024-11-27 12:48:31 +0000

    added github actions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    TOKEN_NAME: 'wackys-token',
+    baseURL: 'http://localhost:prod',
+    apiUrl: 'http://localhost:prod',
+    vapidPublicKey: 'BDkGS2pensSsI7x0gq6ANFtnoeaXImnVGuLJLRshH2ZGwL-8fYu-pgsaMWw-kgIi2BhybwzECmX4y8IoS1L73vA'
+};
