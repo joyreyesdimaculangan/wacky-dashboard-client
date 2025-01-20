a7a2519a1d08b30b1b8e43542e5910c96c47941c
Mark Lowel Montealto <102800815+KuyaGit@users.noreply.github.com>
2025-01-20 16:12:03 +0800

    change environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   TOKEN_NAME: 'wackys-token',
-  baseURL: 'https://wacky-production.up.railway.app',
-  apiUrl: 'https://wacky-production.up.railway.app',
+  baseURL: 'https://wacky-api.tech',
+  apiUrl: 'https://wacky-api.tech',
   vapidPublicKey: 'BDkGS2pensSsI7x0gq6ANFtnoeaXImnVGuLJLRshH2ZGwL-8fYu-pgsaMWw-kgIi2BhybwzECmX4y8IoS1L73vA'
 };
